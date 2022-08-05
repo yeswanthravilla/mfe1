c26e19c5a084a7f882d0228f35167cb3c7897843
yeswanthravilla <yeswanth.ravilla@gmail.com>
2022-08-05 18:50:37 +0530

    initial commit

---
+++ packages1/marketing/config/webpack.dev.js
@@ -0,0 +1,32 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const commonConfig = require("./webpack.common");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const PackageJson = require('../package.json');
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8082,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: 'remoteEntry.js',
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+
+      shared: PackageJson.dependencies,
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
