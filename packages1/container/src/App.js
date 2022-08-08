1ecbfec00a5e6a551a897219a378b0e9c73e42c3
yeswanthravilla <yeswanth.ravilla@gmail.com>
2022-08-08 21:15:00 +0530

    added prefix

--- packages1/container/src/App.js
+++ packages1/container/src/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 import { BrowserRouter  } from 'react-router-dom';
+import { StylesProvider,createGenerateClassName } from "@material-ui/core/styles";
+
+const generateClassname = createGenerateClassName({
+  productionPrefix : 'co' ,
+});
+
 
 
 export default () => {
-  return <BrowserRouter>
+  return <StylesProvider createGenerateClassName = {generateClassname}> 
+<BrowserRouter>
   <div>
     <Header/>
     <MarketingApp />
   </div>;
   </BrowserRouter>
+  </StylesProvider>
 };
