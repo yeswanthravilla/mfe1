9f97ff5ce4fc29a9f9ec3dfbab4dbae1bd57d6ea
yeswanthravilla <yeswanth.ravilla@gmail.com>
2022-08-08 12:18:15 +0530

    dummy commit

--- packages1/marketing/src/bootstrap.js
+++ packages1/marketing/src/bootstrap.js
@@ -6,7 +6,7 @@ const mount = (el) => {
   ReactDOM.render(<App />, el);
 };
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "development" ) {
   const devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
